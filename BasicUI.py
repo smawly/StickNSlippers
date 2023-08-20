5eea32364d6016e6a2400058ce3bc0a0dd814cb4
Enes ERKEN <121829291+Edinbo@users.noreply.github.com>
2023-08-20 14:51:51 +0200

    Update BasicUI.py

--- BasicUI.py
+++ BasicUI.py
@@ -1,43 +1,67 @@
-import sys
-
-import pygame
-import random
+import tkinter as tk
+import ttkbootstrap as ttk
 import time
+import random
 
-pygame.init()
+# Start
+window = ttk.Window(themename="darkly")
+window.geometry("500x350")
+window.title("Sticks N Slippers")
+# Frames
+shop_frame = ttk.Frame()
+name_frame = ttk.Frame()
+main_frame = ttk.Frame()
+fight_frame = ttk.Frame()
+name_frame.pack()
 
-screen_width = 1280
-screen_height = 720
-title = "Sticks n Slippers"
-font = pygame.font.SysFont('Sans', 50)
-clock = pygame.time.Clock()
-icon_image = pygame.image.load("StartButton.png")
-screen = pygame.display.set_mode((screen_width, screen_height))
-icon = pygame.display.set_icon(icon_image)
-title_game = pygame.display.set_caption(title)
-Text = font.render("Input Your Name", True, (0, 0, 0))
-text = ""
-running = True
-while running:
-    screen.fill((255, 255, 255))
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            input_active = True
-            text = ""
-        elif event.type == pygame.KEYDOWN and input_active:
-            if event.key == pygame.K_RETURN:
-                input_active = False
-            elif event.key == pygame.K_BACKSPACE:
-                text = text[:-1]
-            else:
-                text += event.unicode
+# Definitions
+def name_set():
+    name_button.pack(pady=5)
+    name = name_entry.get()
+    main_frame.pack()
+    label.pack()
+    name_frame.destroy()
+    shop_button.pack()
+    fight_button.pack()
 
-    screen.blit(Text, (450, 270))
-    text_surf = font.render(text, True, (12, 122, 12))
-    screen.blit(text_surf, text_surf.get_rect(center=screen.get_rect().center))
-    pygame.display.flip()
-pygame.quit()
+    label_value.set(f"{name} the unbeatable.")
+    print(name)
+
+attack_button = ttk.Button(master=fight_frame, text="Attack")
+heal_button = tk.Button(master=fight_frame, text="Heal")
+
+def Shop():
+    pass
+
+def Fight():
+    main_frame.destroy()
+    shop_button.destroy()
+    fight_button.destroy()
+    fight_frame.pack()
+    attack_button.pack()
+    heal_button.pack()
+
+# Setting Name
+name_value = tk.StringVar()
+name_entry = tk.Entry(master=name_frame, textvariable=name_value, justify="center")
+name_label = tk.Label(master=name_frame, text= "Enter Nickname")
+name_button = tk.Button(master=name_frame, text="Set Name", command=name_set)
+name_label.pack()
+name_entry.pack()
+name_button.pack(pady=5)
+
+
+# Main Menu
+label_value = tk.StringVar()
+label = ttk.Label(master=main_frame, textvariable=label_value)
+shop_button = ttk.Button(master=main_frame, text="Shop", command=Shop())
+fight_button = ttk.Button(master=main_frame, text="Fight", command=Fight)
+# Shop Menu
+
+# In-Game Menu
+
+# Definitions
+
+
+# Run
+window.mainloop()
